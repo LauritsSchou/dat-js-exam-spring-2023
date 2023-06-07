c712c4d9cb32762e85a18067046788572e246214
LauritsSchou <laurits.schou@gmail.com>
2023-06-07 15:49:38 +0200

    Update script.js

--- environment-09/script.js
+++ environment-09/script.js
@@ -1,1 +1,37 @@
 "use strict";
+let posts = [];
+
+window.addEventListener("load", initApp);
+
+async function initApp() {
+  await getPosts();
+  document.querySelector("#all").addEventListener("change", checkFilter);
+}
+
+async function getPosts() {
+  const response = await fetch("posts.json");
+  const data = await response.json();
+  posts = data;
+}
+function showPosts() {
+  const postList = document.querySelector("#posts-list");
+  postList.innerHTML = "";
+  for (const post of posts) {
+    const postListHTML = /*html*/ `
+    
+    <article>
+                    <img src=${post.image} alt=${post.caption} />
+                    <h2>${post.caption}</h2>
+                    <p>Likes: ${post.likes}</p>
+                </article>
+    `;
+    postList.insertAdjacentHTML("beforeend", postListHTML);
+  }
+}
+function checkFilter() {
+  if (document.querySelector("#all").checked) {
+    showPosts();
+  } else {
+    document.querySelector("#posts-list").innerHTML = "";
+  }
+}
